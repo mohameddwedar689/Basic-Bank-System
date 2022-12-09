537ccce740f2973f1c8966bfa78f4423097a581d
mohameddwedar689 <mohameddwedar689@gmail.com>
2022-12-09 23:42:30 +0200

    finish header section

--- index.php
+++ index.php
@@ -5,10 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bank | Home Page</title>
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <!-- custom css file link  -->
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>" type="text/css">
 </head>
 <body>
-    
+    <!-- Header Section Start -->
+    <section class="header">
+        <a href="home.php" class="logo">Bank System.</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">Customers</a>
+            <a href="package.php">Transfer</a>
+            <a href="book.php">Transfer History</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+    <!-- Header Section End -->
+    <!-- custom js file link  -->
+    <script type="text/javascript" src="js/script.js?v=<?php echo time();?>"></script>
 </body>
 </html>
