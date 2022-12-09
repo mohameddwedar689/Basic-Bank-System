9b95ffbd8efa70349a369b5d7b2f71ac1e394748
mohameddwedar689 <mohameddwedar689@gmail.com>
2022-12-09 23:24:49 +0200

    setup file structure

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bank | Home Page</title>
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>" type="text/css">
+</head>
+<body>
+    
+</body>
+</html>
