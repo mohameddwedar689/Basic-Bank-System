b9a51acb4799aa9362005a1d23c7c760e5631e5b
mohameddwedar689 <mohameddwedar689@gmail.com>
2022-12-11 23:41:46 +0200

    finish transfer operation

--- transfer.php
+++ transfer.php
@@ -26,7 +26,84 @@
     </section>
     <!-- Header Section End -->
     <!-- Transfer Section Start-->
-    <h1>Transfer</h1>
+    <section class="transfer">
+        <h1 class="heading-title">Transfer Operation</h1>
+        <div class="container">
+            <form method="POST" class="transfer-form">
+            <?php
+                    //start connection
+                    $connection = mysqli_connect("localhost" , "root" , "" , "bank_db");
+                    //try connection
+                    if($connection -> connect_error){
+                        die("Connection Failed:" . $connection -> connect_error);
+                    }
+
+                    //checking 
+                    if(isset($_POST['submit']))
+                    {
+                        $sender = $_POST['sender'];
+                        $reciver = $_POST['reciver'];
+                        $amount = $_POST['amount'];
+
+                        if($sender <= 0 || $reciver <= 0 || $amount <= 0)
+                        {
+                            echo '<script>alert("Error Massage: Values must be more than zero!")</script>';
+                        }
+                        else
+                        {
+                            //for sender
+                            $sqlGetSender = "SELECT * from customers where id=$sender";
+                            $sender_query = mysqli_query($connection, $sqlGetSender);
+                            $sqlSender = mysqli_fetch_array($sender_query);
+
+                            //for reciver
+                            $sqlGetReciver = "SELECT * from customers where id=$reciver";
+                            $recive_query = mysqli_query($connection, $sqlGetReciver);
+                            $sqlReciver = mysqli_fetch_array($recive_query);
+
+                            if($amount < $sqlSender['balance'])
+                            {
+                                $new_balance_sender = $sqlSender['balance'] - $amount;
+                                $sql = "UPDATE customers set balance=$new_balance_sender where id=$sender";
+                                mysqli_query($connection, $sql);
+
+                                $new_balance_reciver = $sqlReciver['balance'] + $amount;
+                                $sql = "UPDATE customers set balance=$new_balance_reciver where id=$reciver";
+                                mysqli_query($connection, $sql);
+
+
+                                $sender_id = $sqlSender['id'];
+                                $reciver_id = $sqlReciver['id'];
+
+                                $Insertsql = "INSERT INTO `transfer` (`transfer_num`, `sender_id`, `reciver_id`, `amount`, `date`) VALUES ('NULL','$sender_id ','$reciver_id','$amount', current_timestamp())";
+
+                                $insert = mysqli_query($connection, $Insertsql);
+
+                                if($insert)
+                                {
+                                    echo '<script>alert("Successfully Transfering Operation")</script>';
+                                }
+
+                            }
+                            else if($amount >= $sqlSender['balance'])
+                            {
+                                echo '<script>alert("Opps, the amount of money is higher than you have")</script>';
+                            }
+                        }
+                    }
+                ?>
+                <label class="from">Transfer From</label>
+                <input type="number" id="form" name="sender" placeholder="Enter customer Id">
+                <label class="to">Transfer To</label>
+                <input type="number" id="to" name="reciver" placeholder="Enter customer Id">
+                <label class="amount">Amount</label>
+                <input type="number" id="amount" name="amount" placeholder="Enter the amount">
+                <div class="btnholder">
+                    <button class="btn" type="submit" name="submit">Transfer</button>
+                </div>
+            </form>
+        </div>
+    </section>
     <!-- Transfer Section End -->
     <!-- Footer Section Start -->
     <section class="footer">
